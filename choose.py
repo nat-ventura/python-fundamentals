e3f8872d265787fd336e66b9c0c79e5d60c3e35f
Natalie Villasana <navillasa@gmail.com>
2017-10-07 23:58:18 -0400

    adds choosey function

--- choose.py
+++ choose.py
@@ -1,1 +1,13 @@
-# choose.py 
+# choose.py
+# gets two integer sfrom the user (n and k)
+# and computes n choose k
+# aka counts the number of ways you can choose k objects 
+# from a set of n distinct objects
+# and can be computed as n! / (k! * (n-k)!)
+
+def choosey():
+    n = int(input('give me one number please! '))
+    k = int(input('give me another number please! '))
+
+choosey()
+ 
