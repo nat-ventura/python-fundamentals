c5d5d6fd713bccbf940059c9f162f75be5355fa5
Natalie Villasana <navillasa@gmail.com>
2017-10-07 23:06:59 -0400

    adds print statements to describe process in each iteration

--- sum.py
+++ sum.py
@@ -1,2 +1,11 @@
 # sum.py
-# Computes the sum of the first 4 positive integers
+# Computes the sum of the first 4 positive integer
+
+sum = 0
+
+for i in range(1, 5):
+    print 'sum is', sum
+    print 'index is', i
+    print 'adding', sum, '+', i
+    sum = sum + i
+print 'the sum of the first', i, 'positive integers is', sum
