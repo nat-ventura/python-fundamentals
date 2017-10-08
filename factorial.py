b687a81fe5582df3d5bc84864fea8d6f7eeb7f9c
Natalie Villasana <navillasa@gmail.com>
2017-10-07 23:49:15 -0400

    fixes to make sure we're not multiplying by zero, lord

--- factorial.py
+++ factorial.py
@@ -6,13 +6,15 @@ def facty():
     inputNum = int(input('please enter a positive integer' +
                 ' and i will calculate the product counting' +
                 ' up to your integer.\nalso i\'ll be your' +
-                ' friend.'))
-    product = 0
+                ' friend: '))
+    product = inputNum
 
-    if inputNum < 0:
-        for i in range(inputNum):
+    if inputNum > 0:
+        for i in range(1, inputNum + 1):
             print 'product is ', product
             print 'multiplying index', i, '* product', product
             product = i * product
+        print ('lol the first time i did this, i totally didn\'t' +
+        ' start from index 0 i promise.')
 
 facty()
