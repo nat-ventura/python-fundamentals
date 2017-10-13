b285dbf1622f72c064689e8b54a8f4013a2b7b1f
Natalie Villasana <navillasa@gmail.com>
2017-10-12 22:24:00 -0400

    adds prompt to secondconverter

--- secondconverter.py
+++ secondconverter.py
@@ -2,6 +2,10 @@
 # translate seconds into more readable hours, mins, secs
 
 def second_converter():
-    print 'hi and welcome to the second converter'
+    print 'hi and welcome to the second converter.' +
+    ' if you give me a big amount of seconds, i can' +
+    ' convert your amount into hours, minutes, and seconds.'
+
+    sec = int(input('how many seconds would you like to convert?'))
 
 second_converter()
