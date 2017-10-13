4f18cf889ebed0b944b1371e69f982cbe3819816
Natalie Villasana <navillasa@gmail.com>
2017-10-12 21:46:00 -0400

    adds n choose k function :)

--- choose.py
+++ choose.py
@@ -9,5 +9,22 @@ def choosey():
     n = int(input('give me one number please! '))
     k = int(input('give me another number please! '))
 
+    n_fact = 1
+    for i in range(2, n + 1):
+        n_fact = n_fact * i
+
+    k_fact = 1
+    for i in range(2, n + 1):
+        k_fact = k_fact * i
+
+    # calculate n-k
+    selection = n - k
+    nk_fact = 1
+    for i in range(2, selection + 1):
+        nk_fact = nk_fact * i
+
+    choose = n_fact / (k_fact * nk_fact)
+
+    print(n, 'choose', k, 'is:', choose)
+
 choosey()
- 
