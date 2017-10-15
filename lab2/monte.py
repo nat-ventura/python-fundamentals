2585fe2454f3b3a75c6b54eb1d32ad128e3015e0
Natalie Villasana <navillasa@gmail.com>
2017-10-14 23:49:54 -0400

    adds randomized dart-throwing system

--- lab2/monte.py
+++ lab2/monte.py
@@ -6,5 +6,13 @@ import math
 
 def monte():
 
+darts = int(input('you said you wanna throw HOW many darts at me? '))
+hits = 0
+
+for i in range(darts):
+    rand_x = random.uniform(-1.0, 1.0)
+    rand_y = random.uniform(-1.0, 1.0)
+    if (math.sqrt(rand_x * rand_x + rand_y * rand_y) <= 1):
+        hits += 1
 
 monte()
