033bb067d35caf4141426a86f94ab602e68e981a
Natalie Villasana <navillasa@gmail.com>
2017-10-14 23:25:46 -0400

    adds lab2 folder

---
+++ lab2/fibonacci.py
@@ -0,0 +1,11 @@
+# fibonacci.py
+# computes the nth fibonacci number
+
+def fibonacci():
+    print('hi, if you give me a positive integer > 2, then i\'ll tell you' +
+    ' what fibonacci number is in that place.')
+
+    n = int(input('what number? '))
+
+
+fibonacci()
