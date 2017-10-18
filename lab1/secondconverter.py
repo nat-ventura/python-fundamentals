11bc9ea83a6a9c212d30bbcfc095bfaa8079e5f4
Natalie Villasana <navillasa@gmail.com>
2017-10-18 11:14:38 -0400

    fixes string output

--- lab1/secondconverter.py
+++ lab1/secondconverter.py
@@ -2,17 +2,17 @@
 # translate seconds into more readable hours, mins, secs
 
 def second_converter():
-    print 'hi and welcome to the second converter.' +
-    ' if you give me a big amount of seconds, i can' +
-    ' convert your amount into hours, minutes, and seconds.'
+    print ('hi and welcome to the second converter.' +
+      ' if you give me a big amount of seconds, i can' +
+      ' convert your amount into hours, minutes, and seconds.')
 
-    given_sec = int(input('how many seconds would you like to convert?'))
+    given_sec = int(input('how many seconds would you like to convert? '))
 
     minutes = given_sec // 60
     leftover_sec = given_sec % 60
     hours = minutes // 60
     leftover_min = minutes % 60
 
-    print('you gave me ', hours, 'hours,', leftover_min, 'min, and', leftover_sec, 'seconds.')
+    print 'you gave me ', hours, 'hours,', leftover_min, 'minutes, and', leftover_sec, 'seconds.'
 
 second_converter()
